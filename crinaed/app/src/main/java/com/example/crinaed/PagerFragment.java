274975025bfd4a4972e37192d12f496b1aed7275
cricas <cristian.casadei6@studio.unibo.it>
2020-06-25 10:22:26 +0200

    fix tabs layout

--- crinaed/app/src/main/java/com/example/crinaed/PagerFragment.java
+++ crinaed/app/src/main/java/com/example/crinaed/PagerFragment.java
@@ -6,13 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class PagerFragment extends Fragment {
 
@@ -21,19 +22,39 @@ public class PagerFragment extends Fragment {
     private static final int SOCIAL_FRAGMENT = 1;
     private static final int LEARNING_FRAGMENT = 2;
 
-
-
     private ViewPager2 viewPager;
     private FragmentStateAdapter pagerAdapter;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pager, container, false);
 
+        //configure viewPagger2
         viewPager = view.findViewById(R.id.container_page);
         pagerAdapter = new HomePagerAdapter(getChildFragmentManager(),getLifecycle());
         viewPager.setAdapter(pagerAdapter);
+
+        //configure tab layout
+        TabLayout tabs = view.findViewById(R.id.tabs);
+        new TabLayoutMediator(tabs, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                 switch (position)   {
+                     case OBJECTIVE_FRAGMENT:
+                         tab.setText(R.string.tab_objective);
+                         break;
+                     case SOCIAL_FRAGMENT:
+                         tab.setText(R.string.tab_social);
+                         break;
+                     case LEARNING_FRAGMENT:
+                         tab.setText(R.string.tab_learning);
+                         break;
+                     default:
+                         tab.setText("undefine");
+                         break;
+                 }
+            }
+        }).attach();
         return view;
     }
 
@@ -41,15 +62,6 @@ public class PagerFragment extends Fragment {
 
 
     private class HomePagerAdapter extends FragmentStateAdapter{
-
-
-        public HomePagerAdapter(@NonNull FragmentActivity fragmentActivity) {
-            super(fragmentActivity);
-        }
-
-        public HomePagerAdapter(@NonNull Fragment fragment) {
-            super(fragment);
-        }
 
         public HomePagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
             super(fragmentManager, lifecycle);
